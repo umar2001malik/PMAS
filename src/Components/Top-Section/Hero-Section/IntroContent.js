fe2106058d0d310d934abb222b02103394db0505
CodeByMUmar <162801550+CodeByMUmar@users.noreply.github.com>
2024-05-21 07:00:27 -0700

    first commit

--- src/Components/Top-Section/Hero-Section/IntroContent.js
+++ src/Components/Top-Section/Hero-Section/IntroContent.js
@@ -2,26 +2,26 @@ import React from 'react'
 
 
 const IntroContent = () => {
-    return (
-        <div className="flex justify-center items-center  mx-4">
-  <div className="mx-auto md:mx-10 max-w-3xl">
-    <div className="md:mx-0 mx-4 text-center">
-      <h1 className="font-bold text-[#0577B3] md:text-3xl text-2xl ">
-        aradianArray = "Empowering UIIT's Code Worriers"
-      </h1>
-      <p className="my-6 md:w-3/4 mx-auto">
-        
-        A community of passionate learners, creators, and leaders who are driven to make a difference in the world. 
-      </p>
+  return (
+    <div className="flex justify-center items-center  mx-4">
+      <div className="mx-auto md:mx-10 max-w-3xl">
+        <div className="md:mx-0 mx-4 text-center">
+          <h1 className="font-bold text-[#0577B3] md:text-3xl text-2xl ">
+            aradianArray = "Empowering UIIT's Code Worriers"
+          </h1>
+          <p className="my-6 md:w-3/4 mx-auto">
+
+            A community of passionate learners, creators, and leaders who are driven to make a difference in the world.
+          </p>
+        </div>
+
+      </div>
     </div>
-    
-  </div>
-</div>
 
 
 
 
-    )
+  )
 }
 
 export default IntroContent;
